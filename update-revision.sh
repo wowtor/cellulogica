b644e490dc6c71db2c0c85803b1113138a4cadea
wb <wowtorx@gmail.com>
2018-09-20 18:32:45 +0200

    added build date

--- update-revision.sh
+++ update-revision.sh
@@ -4,5 +4,6 @@ STRINGS=app/src/main/res/values/version_info.xml
 
 echo '<resources>
     <string name="revision">080f789ef8c83af8b6b2bb23d5ba2b7f6623fbe7</string>
+    <string name="build_date">'$(date)'</string>
 </resources>
 ' >$STRINGS
